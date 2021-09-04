3b764c98f6f896d5b4d6c7510bece6940b0d3868
Chris Hughes <hughesc4@oregonstate.edu>
2021-09-04 08:45:11 -0700

    task: add ignore config to flake8

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
 
-from app.calc import add
+from app.calc import add, subtract
 
 
-# docker-compose run app sh -c "python manage.py test"
+# docker-compose run app sh -c "python manage.py test && flake8"
 
 class CalcTests(TestCase):
   
@@ -11,3 +11,7 @@ class CalcTests(TestCase):
   def test_add_numbers(self):
     '''Test sum of two numbers'''
     self.assertEqual(add(3,8), 11)
+
+  def test_subtract_numbers(self):
+    '''Test difference of two numbers'''
+    self.assertEqual(subtract(5,11), 6)
